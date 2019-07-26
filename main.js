b91ad2838ad6acfde47cefe2690192ba843e29db
Girmay Hailu Berhe <48474199+berheg@users.noreply.github.com>
2019-07-26 22:49:00 +0200

    database added and data inserted

--- main.js
+++ main.js
@@ -3,15 +3,30 @@ let app = express();
 app.listen(3000, () => console.log('Listening at 3000'));
 app.use(express.static('public'));
 app.use(express.json({limit: 'lmb'}));
+const Datastore = require('nedb');
 const database = new Datastore('database.db');
 database.loadDatabase();
-app.get('/api', (Request,Response) => {
-    Response.json({test});
-})
-app.post('api', (Request,Response) => {
-    const data = Request.body;
+
+app.get('/api', (request, response) => {
+    database.find({}, (err, data) => {
+        if (err) {
+            response.end();
+            return;
+        }
+        response.json(data);
+    });
+});
+app.post('/api', (request, response) => {
+    console.log('I got a request!');
+    console.log(request.body);
+    const data =request.body;
     const timestamp = Date.now();
     data.timestamp = timestamp;
     database.insert(data);
-    Response.json(data);
-});
+    response.json({
+        status: 'success',
+        timestamp: timestamp,
+        latitude: data.lat,
+        longitude: data.lon
+    });
+})
